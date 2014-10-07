9e5c303948f48ce10df92365b1fce10594784980
nj-bay <neal.jacob.415@gmail.com>
2014-10-06 17:43:05 -0700

    Converted album view to Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -275,18 +275,14 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      templateUrl: '/templates/collection.html'
    });
 
+   $stateProvider.state('album', {
+     url: '/album',
+     controller: 'Album.controller',
+     templateUrl: '/templates/album.html'
+
+   });
+
 }]);
-
-// blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-//      $locationProvider.html5Mode(true);
-
-//    $stateProvider.state('collection', {
-//       url: '/collection'
-//       controller: 'Collection.controller',
-//       templateUrl: '/templates/collection.html'
-//    });
-
-// }]);
 
 
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -317,29 +313,41 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
     }
 }]);
 
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
 
+  var hoveredSong = null;
+  var playingSong = null;
 
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
 
-// angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-//   $scope.subText = "Turn the music up!";
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
 
-//   $scope.subTextClicked = function() {
-//     $scope.subText += '!';
-//   };
+  $scope.getSongState = function(song) {
+    if (song === playingSong) {
+      return 'playing';
+    }
 
-//    $scope.albumURLs = [
-//      '/images/album-placeholders/album-1.jpg',
-//      '/images/album-placeholders/album-2.jpg',
-//      '/images/album-placeholders/album-3.jpg',
-//      '/images/album-placeholders/album-4.jpg',
-//      '/images/album-placeholders/album-5.jpg',
-//      '/images/album-placeholders/album-6.jpg',
-//      '/images/album-placeholders/album-7.jpg',
-//      '/images/album-placeholders/album-8.jpg',
-//      '/images/album-placeholders/album-9.jpg',
-//    ];
+    else if (song === hoveredSong) {
+      return 'hovered';
+    }
 
-// }]);
+    return 'default';
+  };
+
+  $scope.playSong = function(song) {
+    playingSong = song;
+  };
+
+  $scope.pauseSong = function(song) {
+    playingSong = null;
+  };
+
+}]);
 
 });
 
